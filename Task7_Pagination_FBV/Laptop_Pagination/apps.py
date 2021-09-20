865579b0c04d736c13789602f85fc67079e4614d
Pratik Bansod <83473640+bansodpratik123@users.noreply.github.com>
2021-09-21 01:10:31 +0530

    Delete Task7_Pagination_FBV directory

--- Task7_Pagination_FBV/Laptop_Pagination/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LaptopPaginationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Laptop_Pagination'
--- Task7_Pagination_FBV/Laptop_Pagination/urls.py
+++
@@ -1,8 +0,0 @@
-from .views import homepage, showlaptops
-
-from django.urls import path
-
-urlpatterns=[
-    path('',homepage, name='home'),
-    path('showlaptop/',showlaptops, name='showlaptop')
-]
